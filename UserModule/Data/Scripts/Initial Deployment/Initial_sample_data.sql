e9f199e231c85236183855155798224dccfcfff2
Dinusha <dinushagammanpila@gmail.com>
2024-02-26 23:42:25 +0530

    Added fail-safe script execution data load conditions

--- UserModule/Data/Scripts/Initial Deployment/Initial_sample_data.sql
+++ UserModule/Data/Scripts/Initial Deployment/Initial_sample_data.sql
@@ -1,15 +1,24 @@
-INSERT INTO User_Role(role_name) VALUES ('SUPERADMIN');
-INSERT INTO User_Role(role_name) VALUES ('ADMIN');
-INSERT INTO User_Role(role_name) VALUES ('USER');
+IF ( SELECT count(*) FROM information_schema.tables WHERE table_catalog ='user_module_db' AND table_name ='User_Role' ) > 0
+BEGIN
+	INSERT INTO User_Role(role_name) VALUES ('SUPERADMIN');
+	INSERT INTO User_Role(role_name) VALUES ('ADMIN');
+	INSERT INTO User_Role(role_name) VALUES ('USER');
+END;
 
-INSERT INTO "USER" (first_name, last_name, email, address, country, user_role) 
-VALUES ('Paul', 'Reynolds', 'paul@gmail.com', 'Colombo', 'Sri Lanka', 1);
-INSERT INTO "USER" (first_name, email, address, country, user_role) 
-VALUES ('Anne', 'anne@gmail.com', 'Colombo', 'Sri Lanka', 2);
-INSERT INTO "USER" (first_name, email, address, country, user_role) 
-VALUES ('John', 'john@gmail.com', 'Colombo', 'Sri Lanka', 3);
+IF ( SELECT count(*) FROM information_schema.tables WHERE table_catalog ='user_module_db' AND table_name ='User' ) > 0
+BEGIN
+	INSERT INTO "User" (first_name, last_name, email, address, country, user_role) 
+	VALUES ('Paul', 'Reynolds', 'paul@gmail.com', 'Colombo', 'Sri Lanka', 1);
+	INSERT INTO "User" (first_name, email, address, country, user_role) 
+	VALUES ('Anne', 'anne@gmail.com', 'Colombo', 'Sri Lanka', 2);
+	INSERT INTO "User" (first_name, email, address, country, user_role) 
+	VALUES ('John', 'john@gmail.com', 'Colombo', 'Sri Lanka', 3);
+END;
 
-INSERT INTO User_Phone_Number(user_id, phone_number) VALUES(10000, 742490323);
-INSERT INTO User_Phone_Number(user_id, phone_number) VALUES(10000, 742434533);
-INSERT INTO User_Phone_Number(user_id, phone_number) VALUES(10001, 742443533);
-INSERT INTO User_Phone_Number(user_id, phone_number) VALUES(10002, 734667323);
+IF ( SELECT count(*) FROM information_schema.tables WHERE table_catalog ='user_module_db' AND table_name ='User_Phone_Number' ) > 0
+BEGIN
+	INSERT INTO User_Phone_Number(user_id, phone_number) VALUES(10000, 742490323);
+	INSERT INTO User_Phone_Number(user_id, phone_number) VALUES(10000, 742434533);
+	INSERT INTO User_Phone_Number(user_id, phone_number) VALUES(10001, 742443533);
+	INSERT INTO User_Phone_Number(user_id, phone_number) VALUES(10002, 734667323);
+END;
